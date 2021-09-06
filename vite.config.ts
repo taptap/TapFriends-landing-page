cbb3bd670e7a13f44d0cadac8519b00d2f60dcfc
sdjdd <i@sdjdd.com>
2021-09-06 15:55:39 +0800

    build: support legacy browsers

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import reactJsx from 'vite-react-jsx';
 import WindiCSS from 'vite-plugin-windicss';
+import legacy from '@vitejs/plugin-legacy';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), reactJsx(), WindiCSS()],
+  plugins: [reactRefresh(), reactJsx(), WindiCSS(), legacy()],
   resolve: {
     alias: {
       assets: path.resolve('./src/assets'),
